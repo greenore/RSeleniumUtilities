a1228d180335827e57c83ff68c1cb83099556669
greenore <tim.hagmann@gmail.com>
2014-08-25 12:45:19 +0200

    renaming

--- envVar.R
+++ envVar.R
@@ -1,3 +1,5 @@
+# Get System information
+#-----------------------
 sysInfo <- function(){
   sysName <<- Sys.info()["sysname"][[1]]
   usrName <<- Sys.info()["user"][[1]]
@@ -24,9 +26,91 @@ getPath <- function(){
   seleniumPath <<- paste0(utilPath, "/bin/seleniumJar/selenium-server-standalone.jar")
 }
 
+# Appending to a file on unix systems
+#------------------------------------
+appendFileUnix <- function(inputTxt, filePath){
+  # If the last line is not empty, add line
+  fileContent <- readLines(filePath)
+  if(fileContent[length(fileContent)] != "") {
+    system(paste0("echo ''", " >> ", filePath), intern=TRUE)
+  }
+    
+  # Add text to file 
+  if(!inputTxt %in% fileContent) {
+    system(paste0("echo '", inputTxt, "'", " >> ", filePath), intern=TRUE)
+    cat(paste0("Succesfully added:\n", inputTxt, "\nto:\n", filePath, "\n"))  
+  } else {
+    warning(paste0("\n\nDid not add:\n", inputTxt, "\nto:\n", filePath, "\n\nText already exists...\n"))
+  }
+}
+
+
+
+# Environmental Variables
+#-------------------------
+getEnvVar <- function(envVarName="Path"){
+  
+  if(sysName == "Windows") {
+    envVarName <- paste0(envVarName, "=")
+    envVar <- shell('set Path', intern=TRUE)
+    envVar <- envVar[grep(envVarName, envVar)]
+    envVar <- unlist(strsplit(envVar, ";"))
+    envVar <- gsub(pattern=envVarName, replacement="", x=envVar)
+    envVar <- gsub(pattern="[\\]", replacement="/", x=envVar)
+  }
+  
+  if(sysName == "Linux") {
+    if(envVarName=="Path") {envVarName <- toupper(envVarName)}
+    envVarName <- paste0("$", envVarName)
+    envVar <- system(paste0('echo ', envVarName), intern=TRUE)
+    envVar <- unlist(strsplit(envVar, ":"))
+  }
+  
+  envVar
+}
+
+checkEnvVar <- function(pathVar){
+  envVar <- getEnvVar()
+
+  envVar <- tolower(gsub(pattern="/", replacement="", x=envVar))
+  pathVar <- tolower(gsub(pattern="/", replacement="", x=pathVar))
+  
+  if(pathVar %in% envVar) {
+    message("The path to Selenium is correctly added to the system environment variables...")
+    TRUE
+  } else {
+    warning(paste0("\n\nSelenium is not found in the path variable...\n",
+                   "execute: setPath() or add the variable manually to the path...\n",
+                   "NOTE: It might be that you need to restart the system for changes to take effect..."))
+    FALSE
+  }
+  
+}
+
+# Setting Path variables
+#-----------------------
+setPath <- function(installPath){
+  
+  if(sysName == "Windows") {
+    shell(paste0("C:/Windows/System32/setx.exe PATH ", installPath), intern=TRUE)
+    message(paste0(installPath, "\nadded to the Windows environment path...\n"))
+    message("It might be necessary to reboot the Computer in order for the changes to take effect!!!")
+  }
+  
+  if(sysName == "Linux") {
+    profilePath <<- paste0("/home/", usrName, "/.profile")
+    appendFileUnix('# Add Selenium to the PATH Environment', profilePath)
+    appendFileUnix(paste0("export PATH=$PATH:", installPath), profilePath)
+    message(paste0("export PATH=$PATH:", installPath, "\nadded to the .profile file...\n"))
+    message("It might be necessary to reboot the Computer in order for the changes to take effect!!!")
+  }
+}
+
+
+# Copying of the Selenium utilities 
+#----------------------------------
 copyUtilities <- function(installIE=TRUE, installChrome=TRUE){
   sysInfo()
-  
   # Remove and create hidden selenium directory
   unlink(file.path(installPath), recursive=TRUE, force=TRUE)
   dir.create(installPath)
@@ -46,74 +130,32 @@ copyUtilities <- function(installIE=TRUE, installChrome=TRUE){
   file.copy(from=seleniumPath, to=RSeleniumBinPath)
 }
 
-appendFile <- function(inputTxt, filePath){
-  # If the last line is not empty, add line
-  fileContent <- readLines(filePath)
-  if(fileContent[length(fileContent)] != "") {
-    system(paste0("echo ''", " >> ", filePath), intern=TRUE)
-  }
-    
-  # Add text to file 
-  if(!inputTxt %in% fileContent) {
-    system(paste0("echo '", inputTxt, "'", " >> ", filePath), intern=TRUE)
-    cat(paste0("Succesfully added:\n", inputTxt, "\nto:\n", filePath, "\n"))  
-  } else {
-    warning(paste0("\n\nDid not add:\n", inputTxt, "\nto:\n", filePath, "\n\nText already exists...\n"))
-  }
-}
 
-setPath <- function(){
 
-  if(sysName == "Windows") {
-    shell(paste0("C:/Windows/System32/setx.exe PATH '", installPath, "'"), intern=TRUE)
-    message(paste0(installPath, "\nadded to the Windows environment path"))
-  }
-  
-  if(sysName == "Linux") {
-    profilePath <<- paste0("/home/", usrName, "/.profile")
-    appendFile('# Add Selenium to the PATH Environment', profilePath)
-    appendFile(paste0("export PATH=$PATH:", installPath), profilePath)
-    warning("Reboot the Computer in order for the changes to take effect!!!")
-  }
-}
 
-checkPath <- function(){
-  
-  if(sysName == "Windows") {
-    pathName <- "Path="
-    envVar <- shell('set Path', intern=TRUE)
-    envVar <- envVar[grep(pathName, envVar)]
-    envVar <- unlist(strsplit(envVar, ";"))
-    envVar <- gsub(pattern=pathName, replacement="", x=envVar)
-    envVar <- gsub(pattern="[\\]", replacement="/", x=envVar)
-  }
 
-  if(sysName == "Linux") {
-    envVar <- system('echo "$PATH"', intern=TRUE)
-    envVar <- unlist(strsplit(envVar, ":"))
-  }
-  envVar <- tolower(gsub(pattern="/", replacement="", x=envVar))
-  selPath <- tolower(gsub(pattern="/", replacement="", x=installPath))
-  
-  if(selPath %in% envVar) {
-    message("The path to Selenium is correctly added to the system environment variables...")
-    TRUE
-  } else {
-    warning(paste0("\n\nSelenium is not found in the path variable...\n",
-                   "execute: setPath() or add the variable manually to the path...\n",
-                   "NOTE: It might be that you need to restart the system for changes to take effect..."))
-    FALSE
-  }
-}
+checkEnvVar(pathVar=installPath)
 
+# java in the path?
+# C:\Program Files\Java
 
 # The Selenium Server was started with 
 # -Dwebdriver.chrome.driver=c:\path\to\your\chromedriver.exe
-
+sysInfo()
 getPath()
-if(!checkPath()){
-  setPath()
-}
+setPath(installPath=installPath)
+
+library(basleR)
+identifier <- ""
+regex <- "[[:alnum:]]{1, }"
+C:\Program Files\Java
+expr <- paste(identifier, regex, sep = "")
+output <- regmatches(input, regexpr(expr, input))
+
+C:/Program Files/Java/jdk1.7.0_21/bin
+cutTxt
+
+setPath()
 checkPath()
 copyUtilities(installIE=TRUE, installChrome=TRUE)
 
