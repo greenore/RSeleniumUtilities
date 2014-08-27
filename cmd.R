b05e5622db5eedd2c9072d5f58053b5e315093a7
greenore <tim.hagmann@gmail.com>
2014-08-27 17:02:16 +0200

    slight reformating

--- cmd.R
+++ cmd.R
@@ -1,9 +1,10 @@
-
+library(RSeleniumUtilities)
 getPath()
 javaPath <- getJavaPath()
 checkEnvVar(pathVar=installPath)
 checkEnvVar(pathVar=javaPath)
 setEnvVar(pathToVar=paste0(installPath, ';"', javaPath,'"'))
 copyUtilities(installIE=TRUE, installChrome=TRUE)
+
 
 rm(list = ls())
