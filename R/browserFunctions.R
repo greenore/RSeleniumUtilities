f2b9719f8227fbc8f6a5578d4013271d9b94b4fc
greenore <tim.hagmann@gmail.com>
2014-11-18 17:23:26 +0100

    upload new functions

---
+++ R/browserFunctions.R
@@ -0,0 +1,113 @@
+#' Use Firefox Profiles
+#' 
+#' \code{firefoxDriver}
+#' A utility function for easy and automated access to the firefox driver. 
+#' @param use_profile 
+#' @param profile_dir Specifies the user data directory, which is where the browser will look for all of its state.
+#' @param profile_name Selects directory of profile to associate with the first browser launched.
+#' @param base_encrypton
+#' @export
+#' @section Detail:
+#' The profile directory is contained in the user directory and by default is named "Default" 
+firefoxDriver <- function(use_profile=FALSE, profile_name="default", profile_dir="default", base_encrypton=TRUE){
+    startServer(); Sys.sleep(2)
+  
+  # System information
+  sys.info <- sysInfo()
+  
+  if(use_profile & profile_dir == "default"){
+    if(sys.info$sys_name == "Windows") {
+      profile_dir <- paste0("C:/Users/", sys.info$usr_name, "/AppData/Roaming/Mozilla/Firefox/Profiles")
+    }
+    
+    if(sys.info$sys_name == "Linux") {
+      profile_dir <- paste0("/home/", sys.info$usr_name, "/.mozilla/firefox")
+    }
+    
+    prof_files <- list.files(profile_dir)
+    expr <- paste0("[[:alnum:]]{1, }", ".", profile_name)
+    prof_file <- regmatches(prof_files, regexpr(expr, prof_files))
+    profDir <- paste0(profile_dir, "/", prof_file)
+    fprof <- getFirefoxProfile(profDir, useBase=base_encrypton)
+    remDr <- remoteDriver(browserName='firefox', extraCapabilities=fprof)
+    
+    if(length(prof_file) == 0){
+      warning(paste0('NO PROFILE FOLDER UNDER THE NAME: "', profile_name, '" ...'))
+    } else {
+      remDr$open(); remDr$maxWindowSize()
+      remDr
+    }
+    
+  } else {
+    remDr <- remoteDriver(browserName='firefox')
+    remDr$open(); remDr$maxWindowSize()
+    remDr
+  }
+  
+}
+
+#' Use Chrome Profiles
+#' 
+#' \code{chromeDriver}
+#' A utility function for easy and automated access to the Chrome driver. 
+#' @param use_profile 
+#' @param profile_dir Specifies the user data directory, which is where the browser will look for all of its state.
+#' @param profile_name Selects directory of profile to associate with the first browser launched.
+#' @param internal_testing 
+#' @export
+#' @section Detail:
+#' The profile directory is contained in the user directory and by default is named "Default" 
+chromeDriver <- function(use_profile=FALSE, profile_name="Default",
+                         profile_dir="Default", internal_testing=FALSE){
+  startServer(); Sys.sleep(2)
+
+  # System information
+  sys.info <- sysInfo()
+  
+  # Find profile path according to OS
+  if(use_profile & profile_dir == "Default"){
+    if(sys.info$sys_name == "Windows") {
+      profile_dir <- paste0("C:/Users/", sys.info$usr_name, "/AppData/Local/Google/Chrome/User Data")
+    }
+    
+    if(sys.info$sys_name == "Linux") {
+      profile_dir <- paste0("/home/", sys.info$usr_name, "/.config/google-chrome")
+    }
+  }
+  
+  # Set chrome options (extraCapabilities)
+  args <- NULL
+  if(use_profile){
+    args <- append(args, list(paste0('--user-data-dir=', profile_dir), paste0('--profile-directory=', profile_name)))
+  }
+  
+  if(internal_testing){
+    args <- append(args, list('--test-type'))
+  }
+  
+  if(is.null(args)){
+    cOptions <- list(NULL)
+  } else {
+    cOptions <- list(chromeOptions = list(args = args))    
+  }
+  
+  # Set remote drive according to chrome settings
+  remDr <- remoteDriver(browserName='chrome', extraCapabilities=cOptions)    
+  remDr$open()
+  remDr$maxWindowSize()
+  remDr
+}
+
+#' Use Internet Explorer
+#' 
+#' \code{ieDriver}
+#' A utility function for easy and automated access to the Chrome driver. 
+#' @export
+#' @section Detail:
+ieDriver <- function(){
+  startServer(); Sys.sleep(2)
+  remDr <- remoteDriver(browserName='internet explorer')    
+  remDr$open()
+  remDr$maxWindowSize()
+  remDr
+}
--- R/helperFunctions.R
+++ R/helperFunctions.R
@@ -1,113 +1,123 @@
-#' Use Firefox Profiles
-#' 
-#' \code{firefoxDriver}
-#' A utility function for easy and automated access to the firefox driver. 
-#' @param use_profile 
-#' @param profile_dir Specifies the user data directory, which is where the browser will look for all of its state.
-#' @param profile_name Selects directory of profile to associate with the first browser launched.
-#' @param base_encrypton
+# Helper Functions 1
+#-------------------
+
 #' @export
-#' @section Detail:
-#' The profile directory is contained in the user directory and by default is named "Default" 
-firefoxDriver <- function(use_profile=FALSE, profile_name="default", profile_dir="default", base_encrypton=TRUE){
-    startServer(); Sys.sleep(2)
-  
-  # System information
-  sys.info <- sysInfo()
-  
-  if(use_profile & profile_dir == "default"){
-    if(sys.info$sys_name == "Windows") {
-      profile_dir <- paste0("C:/Users/", sys.info$usr_name, "/AppData/Roaming/Mozilla/Firefox/Profiles")
-    }
-    
-    if(sys.info$sys_name == "Linux") {
-      profile_dir <- paste0("/home/", sys.info$usr_name, "/.mozilla/firefox")
-    }
-    
-    prof_files <- list.files(profile_dir)
-    expr <- paste0("[[:alnum:]]{1, }", ".", profile_name)
-    prof_file <- regmatches(prof_files, regexpr(expr, prof_files))
-    profDir <- paste0(profile_dir, "/", prof_file)
-    fprof <- getFirefoxProfile(profDir, useBase=base_encrypton)
-    remDr <- remoteDriver(browserName='firefox', extraCapabilities=fprof)
-    
-    if(length(prof_file) == 0){
-      warning(paste0('NO PROFILE FOLDER UNDER THE NAME: "', profile_name, '" ...'))
-    } else {
-      remDr$open(); remDr$maxWindowSize()
-      remDr
-    }
-    
-  } else {
-    remDr <- remoteDriver(browserName='firefox')
-    remDr$open(); remDr$maxWindowSize()
-    remDr
-  }
-  
+# Werden für Schritt 1 benoetigt (Fahrzeug, Lenker und Deckung)
+activateDropDown <- function(idButton, idMenu){
+  element1 <- remDr$findElement(using = "id", value = idButton)
+  element2 <- remDr$findElement(using = "id", value = idMenu)
+  while(element2$isElementDisplayed() == F){element1$clickElement(); Sys.sleep(1)}
+  element2
 }
 
-#' Use Chrome Profiles
-#' 
-#' \code{chromeDriver}
-#' A utility function for easy and automated access to the Chrome driver. 
-#' @param use_profile 
-#' @param profile_dir Specifies the user data directory, which is where the browser will look for all of its state.
-#' @param profile_name Selects directory of profile to associate with the first browser launched.
-#' @param internal_testing 
 #' @export
-#' @section Detail:
-#' The profile directory is contained in the user directory and by default is named "Default" 
-chromeDriver <- function(use_profile=FALSE, profile_name="Default",
-                         profile_dir="Default", internal_testing=FALSE){
-  startServer(); Sys.sleep(2)
-
-  # System information
-  sys.info <- sysInfo()
-  
-  # Find profile path according to OS
-  if(use_profile & profile_dir == "Default"){
-    if(sys.info$sys_name == "Windows") {
-      profile_dir <- paste0("C:/Users/", sys.info$usr_name, "/AppData/Local/Google/Chrome/User Data")
-    }
-    
-    if(sys.info$sys_name == "Linux") {
-      profile_dir <- paste0("/home/", sys.info$usr_name, "/.config/google-chrome")
-    }
-  }
-  
-  # Set chrome options (extraCapabilities)
-  args <- NULL
-  if(use_profile){
-    args <- append(args, list(paste0('--user-data-dir=', profile_dir), paste0('--profile-directory=', profile_name)))
-  }
-  
-  if(internal_testing){
-    args <- append(args, list('--test-type'))
-  }
-  
-  if(is.null(args)){
-    cOptions <- list(NULL)
-  } else {
-    cOptions <- list(chromeOptions = list(args = args))    
-  }
-  
-  # Set remote drive according to chrome settings
-  remDr <- remoteDriver(browserName='chrome', extraCapabilities=cOptions)    
-  remDr$open()
-  remDr$maxWindowSize()
-  remDr
+getTxt <- function(element, split){
+  elementList <- element$getElementText()
+  txt <- unlist(strsplit(unlist(elementList), split = split, fixed = T))
+  txt
 }
 
-#' Use Internet Explorer
-#' 
-#' \code{ieDriver}
-#' A utility function for easy and automated access to the Chrome driver. 
 #' @export
-#' @section Detail:
-ieDriver <- function(){
-  startServer(); Sys.sleep(2)
-  remDr <- remoteDriver(browserName='internet explorer')    
-  remDr$open()
-  remDr$maxWindowSize()
-  remDr
+selectLinkTxt <- function(element, linkTxt){
+  element2 <- element$findChildElement(using = "link text", value = linkTxt)
+  element2$clickElement()
+}
+
+#' @export
+# Minimize string distance (Damerau-Levenshtein Method)
+minStringDistance <- function(targ_var, source_var, method='osa'){
+  require(transformR)
+  
+  targ_var <- changeUmlaute(tolower(targ_var))
+  targ_var <- gsub(' ', '', targ_var)     # rm whitespace
+  targ_var <- gsub('[.]', '', targ_var)   # rm dots
+  
+  source_var <- changeUmlaute(tolower(source_var))
+  source_var <- gsub(' ', '', source_var)     # rm whitespace
+  source_var <- gsub('[.]', '', source_var)   # rm dots
+  
+  char_dist <- stringdist(targ_var, source_var, method=method)
+  char_dist
+}
+
+#' @export
+# Select minimized string
+selectMinString <- function(source_var, char_dist, select_first=FALSE){
+  min_string <- source_var[char_dist %in% min(char_dist)]
+  
+  if(length(min_string) != 1){
+    warning("There is more than 1 equivalent string to chose from...")
+    
+    if(select_first){
+      min_string <- min_string[1]
+      message(paste0("The first one was chosen: ", min_string))
+    }
+  }
+  
+  min_string
+}
+
+#' @export
+# Stop scraping and write to profile
+stopScraping <- function(stop_cond, csv_path, error_message, found_message=FALSE){
+  if(stop_cond){
+    df.merge <- read.csv2(csv_path, sep = ';', header = T, stringsAsFactors = F, encoding = 'latin1')[1, ]
+    df.merge[, 1:length(df.merge)] <- error_message
+    df.merge$ID <- ID
+    write.table(df.merge, file=csv_path, append=T, col.names=F, row.names=F, sep=';')
+    message(paste0("ERROR: ", error_message, "..."))
+    try({stop}, silent=TRUE)
+  } else {
+    if(found_message){
+      message("found item...")
+    }
+  }
+}
+
+#' @export
+# Wait until the web element is present
+waitForElement <- function(using, value, nTrials=100, untilItExists=TRUE,
+                           give_message=TRUE){
+  try({
+    counter <- 0
+    
+    # Check until the element exists
+    if(untilItExists == TRUE){
+      elementExists <- FALSE
+      
+      while(counter < nTrials && elementExists == FALSE) {
+        message("Waiting...")
+        Sys.sleep(1)
+        elementExists <- !is.null(try(remDr$findElement(using=using, value=value), silent = TRUE))
+        counter <- sum(counter, 1)
+      }
+      
+      if(elementExists == TRUE & give_message == TRUE){
+        message(paste0("Success...\nThe element: '", value, "' is found..."))
+      }
+    }
+    
+    # Check until the element is gone
+    if(untilItExists == FALSE){
+      Sys.sleep(1)
+      nElements <- length(remDr$findElements(using=using, value=value))
+      
+      while(counter < nTrials && nElements > 0) {
+        if(give_message == TRUE){
+          message(paste0("Waiting...", " Still ", nElements, " elements left"))
+        }
+        Sys.sleep(1)
+        nElements <- length(remDr$findElements(using=using, value=value))
+        counter <- sum(counter, 1)
+      }
+      
+      if(nElements == 0 & give_message == TRUE){
+        message(paste0("Success...\nThe element: '", value, "' is no longer present..."))
+      }
+    }
+    
+    if(counter == nTrials){
+      stop(paste0("Time limit is up, i.e., more than ", nTrials, " trials have passed..."))
+    }
+  }, silent=TRUE)
 }
